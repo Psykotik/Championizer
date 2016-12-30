19419f2cdb1b9a87bd577248120aa8062ff41ca6
Jordan Lambert <lambert.jord@gmail.com>
2016-12-30 19:34:28 +0100

    [Update] IHM first draft

--- app/src/main/java/fr/jordanlambert/championizer/MainActivity.java
+++ app/src/main/java/fr/jordanlambert/championizer/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,11 +19,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button randomization = (Button) findViewById(R.id.button_randomization);
+        randomization.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Clicked on the randomisation button", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
